62775ce619807da0d74ca92e71b23806a122095f
A7Lavinraj <lavinrajmohan@gmail.com>
2025-08-26 14:57:57 +0530

    feat: Implement offer exchange mechanism

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/big"
@@ -11,25 +12,75 @@ import (
 )
 
 type Client struct {
-	conn *websocket.Conn
+	UserId string
+	conn   *websocket.Conn
 }
 
 type Message struct {
-	Type string `json:"type"`
-	Data any    `json:"data"`
+	Type   string `json:"type"`
+	Data   any    `json:"data"`
+	UserId string `json:"userId"`
 }
 
 var ClientConns = map[string]*Client{}
+
+func generateNumericID() string {
+	n, _ := rand.Int(rand.Reader, big.NewInt(999999))
+	return fmt.Sprintf("%06d", n)
+}
+
+func (c *Client) run() {
+	defer c.conn.Close()
+
+	ClientConns[c.UserId] = c
+
+	if err := c.conn.WriteJSON(Message{Type: "INITIAL_CONNECTION", Data: c.UserId}); err != nil {
+		log.Println("Unable to send initial message")
+	}
+
+	for {
+		mt, msg, err := c.conn.ReadMessage()
+		if err != nil || mt == websocket.CloseMessage {
+			break
+		}
+
+		var message Message
+		if err := json.Unmarshal(msg, &message); err != nil {
+			log.Printf("JSON parse error: %v", err)
+		} else {
+			if message.Type == "PEER_CONNECTION_REQUEST" {
+				Client := ClientConns[message.UserId]
+				dataBytes, err := json.Marshal(Message{Type: "PEER_CONNECTION_REQUEST", Data: message.Data, UserId: c.UserId})
+				if err != nil {
+					fmt.Printf("Failed to marshal message.Data: %v\n", err)
+					continue
+				}
+
+				if err := Client.conn.WriteMessage(websocket.TextMessage, dataBytes); err != nil {
+					fmt.Printf("Failed to transfer PEER_CONNECTION_REQUEST to userId: %s\n", message.UserId)
+				}
+			} else if message.Type == "PEER_CONNECTION_RESPONSE" {
+				Client := ClientConns[message.UserId]
+				dataBytes, err := json.Marshal(Message{Type: "PEER_CONNECTION_RESPONSE", Data: message.Data, UserId: c.UserId})
+				if err != nil {
+					fmt.Printf("Failed to marshal message.Data: %v\n", err)
+					continue
+				}
+
+				if err := Client.conn.WriteMessage(websocket.TextMessage, dataBytes); err != nil {
+					fmt.Printf("Failed to transfer PEER_CONNECTION_REQUEST to userId: %s\n", message.UserId)
+				}
+			} else {
+				fmt.Printf("Invalid message type %s", message.Type)
+			}
+		}
+	}
+}
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin:     func(r *http.Request) bool { return true },
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-}
-
-func generateNumericID() string {
-	n, _ := rand.Int(rand.Reader, big.NewInt(999999))
-	return fmt.Sprintf("%06d", n)
 }
 
 func handleWSConnections(w http.ResponseWriter, r *http.Request) {
@@ -39,25 +90,7 @@ func handleWSConnections(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var uuid = generateNumericID()
-	ClientConns[uuid] = &Client{conn: conn}
-
-	if err := conn.WriteJSON(Message{Type: "INITIAL_CONNECTION", Data: uuid}); err != nil {
-		log.Println("Unable to send initial message")
-	}
-
-	for {
-		mt, msg, err := conn.ReadMessage()
-		if err != nil || mt == websocket.CloseMessage {
-			break
-		}
-
-		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
-			break
-		}
-	}
-
-	conn.Close()
+	go (&Client{UserId: generateNumericID(), conn: conn}).run()
 }
 
 func main() {
--- public/index.js
+++ public/index.js
@@ -1,5 +1,14 @@
 const SOCKET_CONNECTION_URL = "ws://localhost:8080/ws";
 
+const rtc = new RTCPeerConnection({
+  iceCandidatePoolSize: 10,
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+});
+
 class WebSocketSingleton {
   constructor(url) {
     if (WebSocketSingleton.instance) {
@@ -16,12 +25,40 @@ class WebSocketSingleton {
       console.log(`Socket connected to ${this.url}`);
     };
 
-    this.socket.onmessage = (e) => {
+    this.socket.onmessage = async (e) => {
       console.log("Socket receive message: ", e.data);
 
       const prasedData = JSON.parse(e.data);
       if (prasedData.type === "INITIAL_CONNECTION") {
         document.querySelector(".userid").innerHTML = prasedData.data;
+      } else if (prasedData.type === "PEER_CONNECTION_REQUEST") {
+        console.log(prasedData.data);
+        const isPermissionGrated = confirm(
+          "A request has arrived, You wanna connect?",
+        );
+
+        if (isPermissionGrated) {
+          let offer = prasedData.data;
+          await rtc.setRemoteDescription(offer);
+
+          let answer = await rtc.createAnswer();
+          await rtc.setLocalDescription(answer);
+
+          this.send({
+            type: "PEER_CONNECTION_RESPONSE",
+            userId: prasedData.userId,
+            data: answer,
+          });
+        }
+      } else if (prasedData.type === "PEER_CONNECTION_RESPONSE") {
+        console.log("Add answer triggerd");
+
+        let answer = prasedData.data;
+        console.log("answer:", answer);
+
+        if (!rtc.currentRemoteDescription) {
+          rtc.setRemoteDescription(answer);
+        }
       }
     };
 
@@ -40,7 +77,7 @@ class WebSocketSingleton {
 
   send(data) {
     if (this.socket.readyState === WebSocket.OPEN) {
-      this.socket.send(data);
+      this.socket.send(JSON.stringify(data));
     } else {
       throw new Error("WebSocket is not open");
     }
@@ -49,7 +86,18 @@ class WebSocketSingleton {
 
 window.addEventListener("load", () => {
   try {
-    new WebSocketSingleton(SOCKET_CONNECTION_URL);
+    const ws = new WebSocketSingleton(SOCKET_CONNECTION_URL);
+    document
+      .querySelector(".action__button")
+      .addEventListener("click", async () => {
+        const offer = await rtc.createOffer();
+        await rtc.setLocalDescription(offer);
+        ws.send({
+          type: "PEER_CONNECTION_REQUEST",
+          data: offer,
+          userId: document.querySelector(".action__input").value,
+        });
+      });
   } catch (err) {
     console.error(err);
   }
